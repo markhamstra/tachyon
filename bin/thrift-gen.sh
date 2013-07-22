6c34a9d7bbcc8714fd1430e5b760db961361fe2f
Mikhail Bautin <mbautin@gmail.com>
2013-07-22 15:20:32 -0700

    Ensure the correct Thrift version is installed and remove .deb dependency on JDK

--- bin/thrift-gen.sh
+++ bin/thrift-gen.sh
@@ -11,4 +11,17 @@ bin=`cd "$( dirname "$0" )"; pwd`
 
 rm -rf $bin/../src/main/java/tachyon/thrift
 
-thrift --gen java -out $bin/../src/main/java/. $bin/../src/thrift/tachyon.thrift
+thrift_version=0.7.0
+thrift_binary=/usr/share/thrift-${thrift_version}/bin/thrift
+if [ ! -f "${thrift_binary}" ]; then
+  thrift_binary=thrift
+fi
+
+if [ "$( ${thrift_binary} -version )" != "Thrift version ${thrift_version}" ]; then
+  echo "Incorrect Thrift version installed, ${thrift_version} required" >&2
+  echo "Thrift binary: ${thrift_binary}" >&2
+  exit 1
+fi
+
+${thrift_binary} --gen java -out $bin/../src/main/java/. $bin/../src/thrift/tachyon.thrift
+
